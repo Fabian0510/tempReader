a77e782e76259f6460cd75a41ee3e5314d558fc9
Geoff Jones <geoffyj86@gmail.com>
2014-08-25 08:43:57 +0900

    Takes input from user

--- readTemp.py
+++ readTemp.py
@@ -1,4 +1,5 @@
 # This is my core temperature reader
-def readTemp(test):
-  return "That worked, " + test
-print readTemp("Geoff")
+def readTemp():
+    name = raw_input("What's yo name?")
+    return "That worked, " + name
+print readTemp()
